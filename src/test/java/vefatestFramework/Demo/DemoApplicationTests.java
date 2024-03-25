3de772fd0a7e9d6466a60d0cfcb1815405034f44
Rojbin Agtas <rojbinagtas@gmail.com>
2024-03-25 15:40:13 +0300

    Add files via upload

---
+++ src/test/java/vefatestFramework/Demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package vefatestFramework.Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
